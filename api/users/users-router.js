39025af587950b2750ddb1aa0e94b153d9cefb66
Jesse <eoshout@Gmail.com>
2021-09-01 19:09:43 -0400

    mvp wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const {validateUserId } = require("../middleware/middleware")
-// You will need `users-model.js` and `posts-model.js` both
-const Users = require("./users-model.js")
+const {validateUserId,validateUser,validatePost } = require("../middleware/middleware")
 
-// The middleware functions also need to be required
+const User = require("./users-model.js")
+const Post = require('../posts/posts-model')
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  Users.get()
+  User.get()
   .then(users => {
     res.status(200).json(users)
   })
@@ -15,22 +15,23 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
   res.json(req.users)
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
-  
+router.post('/', validateUser, (req, res, next) => {
+  User.insert({ name: req.name })
+  .then(newUser => {
+    res.status(201).json(newUser)
+  })
+  .catch(next)
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId,validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   console.log(req.user)
+  console.log(req.name)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
@@ -45,11 +46,12 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   console.log(req.user)
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   console.log(req.user)
+  console.log(req.text)
 });
 
 // do not forget to export the router
@@ -57,7 +59,8 @@ router.use((err, req, res, next) => { // we plug it AFTER the endpoints
   console.log(err.message)
   res.status(err.status || 500).json({
     message: err.message,
-    customMessage: 'Something bad inside the hubs router!'
+    customMessage: 'Something bad inside the Users router!',
+    stack: err.stack,
   })
 });
 
